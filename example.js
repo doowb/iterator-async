84748fb3c2fa8b82cd8d701b7c08c8776bac58cc
jonschlinkert <jon.schlinkert@sellside.com>
2015-06-17 14:18:32 -0400

    make example a little more approachable

--- example.js
+++ example.js
@@ -1,11 +1,27 @@
 var fs = require('fs');
+
 var iterator = require('./');
+
+
 var stack = [
-  function (fp, next) { return fs.readFile(fp, 'utf8', next); },
-  function (contents, next) { return next(null, JSON.parse(contents)); }
+  function read(fp, next) {
+    return fs.readFile(fp, 'utf8', next);
+  },
+  function parse(str, next) {
+    return next(null, JSON.parse(str));
+  },
+  function addStuff(obj, next) {
+    obj.foo = 'bar';
+    return next(null, obj);
+  }
 ];
+
+
 var readJSON = iterator(stack);
 readJSON('./package.json', function (err, pkg) {
   if (err) console.error(err);
-  console.log(pkg);
+  console.log(pkg.name);
+  //=> 'iterator-async'
+  console.log(pkg.foo);
+  //=> 'bar'
 });
